8c06d82d39532b447585d7e54ef58768e2106eab
S-Ventura <bohemian_rapsody@hotmail.es>
2019-04-13 06:53:27 -0500

    Pruebas

---
+++ programa.f
@@ -0,0 +1,8 @@
+  program pruebagit
+
+    implicit none
+    print*, 'Selene Nyan :3'
+    print*, 'Prueba'
+
+
+  end program pruebagit
---
+++ programa1.f
@@ -0,0 +1,7 @@
+  program programaprueba1
+
+    implicit none
+    print*, 'Hola otra vez'
+
+  end program programaprueba1
+  
